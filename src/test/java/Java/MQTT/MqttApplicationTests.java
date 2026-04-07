38e614a8049ffe37bf865167c78fb3105e6db0e2
김동빈 <kdobi87@naver.com>
2026-04-07 09:28:24 +0900

    first commit

---
+++ src/test/java/Java/MQTT/MqttApplicationTests.java
@@ -0,0 +1,13 @@
+package Java.MQTT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
